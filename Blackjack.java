d24d316d55505c73a1d441850858d8a5ad3c4182
Marco Mangan <marco.mangan@pucrs.br>
2022-05-13 10:49:49 -0300

    Acrescentei cartas do computador..

--- Blackjack.java
+++ Blackjack.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+import java.util.Random;
 
 /**
  * Escreva uma descrição da classe Blackjack aqui.
@@ -7,4 +9,19 @@
  */
 public class Blackjack
 {
+    public static void main(String[] args) {
+        Scanner sc;
+        Random r;
+        int c1, c2;
+        
+        r = new Random();
+        
+        c1 = 1 + r.nextInt(13);
+        c2 = 1 + r.nextInt(13);
+        
+        System.out.println(c1);
+        System.out.println(c1);
+        System.out.println(c1 + c2);
+        
+    }
 }
