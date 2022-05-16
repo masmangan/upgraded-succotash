1fc8e8371b11e7467db1813ce21b6b239022af25
Marco Mangan <marco.mangan@pucrs.br>
2022-05-16 10:19:15 -0300

    Rotina com valores corretos de J, Q e K. #4

--- Blackjack.java
+++ Blackjack.java
@@ -11,7 +11,18 @@ public class Blackjack
     public static int gerarCarta(Random r) {
         return 1 + r.nextInt(13);
     }
+
+    public static int valor(int c) {
+        if (c > 10)
+            return 10;
+        else
+            return c;
+    }
     
+    public static int somarCartas(int c1, int c2, int c3) {
+        return valor(c1) + valor(c2) + valor(c3);
+    }
+   
     public static void main(String[] args) {
         Random r;
         int c1, c2, c3;
@@ -31,13 +42,13 @@ public class Blackjack
         System.out.printf("Carta 1: %d%n", c1);
         System.out.printf("Carta 2: %d%n", c2);
         System.out.printf("Carta 3: %d%n", c3);
-        System.out.printf("Total  : %d%n", c1 + c2 + c3);
+        System.out.printf("Total  : %d%n", somarCartas(c1, c2, c3));
        
         System.out.printf("HUMANO%n");
         System.out.printf("Carta 1: %d%n", j1);
         System.out.printf("Carta 2: %d%n", j2);
         System.out.printf("Carta 3: %d%n", j3);
-        System.out.printf("Total  : %d%n", j1 + j2 + j3);
+        System.out.printf("Total  : %d%n", somarCartas(j1, j2, j3));
         
     }
 }
