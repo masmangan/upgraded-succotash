2d5aeeef55dc8103d68bdba623ee1ee27ed49772
Marco Mangan <marco.mangan@pucrs.br>
2022-05-16 10:46:42 -0300

    Terceira carta opcional, por escolha do jogador. #7

--- Blackjack.java
+++ Blackjack.java
@@ -1,4 +1,5 @@
 import java.util.Random;
+import java.util.Scanner;
 
 /**
  * Escreva uma descrição da classe Blackjack aqui.
@@ -24,10 +25,14 @@ public class Blackjack
     }
    
     public static void main(String[] args) {
+        Scanner sc;
         Random r;
         int c1, c2, c3;
         int j1, j2, j3;
+        String resposta;
+        boolean hitme;
         
+        sc = new Scanner(System.in);
         r = new Random();
         
         c1 = gerarCarta(r);
@@ -36,7 +41,26 @@ public class Blackjack
         
         j1 = gerarCarta(r);
         j2 = gerarCarta(r);
-        j3 = gerarCarta(r);
+        
+        System.out.printf("COMPUTADOR%n");
+        System.out.printf("Carta 1: %d%n", c1);
+        
+        System.out.printf("HUMANO%n");
+        System.out.printf("Carta 1: %d%n", j1);
+        System.out.printf("Carta 2: %d%n", j2);
+
+        System.out.printf("%nDeseja uma terceira carta? (sim/NÃO)%n");
+        resposta = sc.nextLine();
+        
+        if ("sim".equalsIgnoreCase(resposta))
+            hitme = true;
+        else
+            hitme = false;
+        
+        if (hitme)
+            j3 = gerarCarta(r);
+        else
+            j3 = 0;
         
         System.out.printf("COMPUTADOR%n");
         System.out.printf("Carta 1: %d%n", c1);
@@ -47,8 +71,11 @@ public class Blackjack
         System.out.printf("HUMANO%n");
         System.out.printf("Carta 1: %d%n", j1);
         System.out.printf("Carta 2: %d%n", j2);
-        System.out.printf("Carta 3: %d%n", j3);
+        if (hitme)
+            System.out.printf("Carta 3: %d%n", j3);
         System.out.printf("Total  : %d%n", somarCartas(j1, j2, j3));
         
+        
+        sc.close();
     }
 }
